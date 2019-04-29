e61424b2a33a605f8553346c418ac4be53e798bc
Jackie <554586805@qq.com>
2019-04-29 20:27:18 +0800

    finish footer

--- html.js
+++ html.js
@@ -1,11 +1,9 @@
 
 var Footer = {
     init:function(){
-        this.bind();
         this.isToStart = true;
         this.isToStart = false;
-    },
-    bind:function(){
+        this.isAnimate = false;/*防止点击过快出现bug*/
         this.render();
     },
     render:function(){
@@ -15,7 +13,7 @@ var Footer = {
             dataType:'json'
         }).done(function(data){
             data.channels.forEach(function(item){
-                var $html = `<div class='song'>
+                var $html = `<div class='song' data-id=${item.channel_id}>
                 <div class='cover' style="background:url('${item.cover_small}') no-repeat center center"></div>
                 <p>${item.name}</p>
                 </div>`
@@ -23,36 +21,55 @@ var Footer = {
             })
             var _this = this;
             var count = Math.floor($('.box').outerWidth(true)/$('.song').outerWidth(true));
-
-                $('.rightBtn').on('click',function(){
+  
+                $('.rightBtn').on('click',function(){ 
+                    console.log(!_this.isAnimate);
                     console.log($('.album').css('left'))
-                    if(!_this.isToEnd){
-                        $('.album').animate({
-                            left:'-='+ count*$('.song').outerWidth(true)
-                        },400,function(){
-                            if((parseFloat($('.box').width())-parseFloat($('.album').css('left'))) >= $('.album').outerWidth(true)){
-                                console.log('over');
-                                _this.isToEnd = true;
-                            }
-                        })
-                        _this.isToStart = false;  
-                    }
+                    if(!_this.isAnimate){
 
+                        if(!_this.isToEnd){
+                            _this.isAnimate = true;
+                            $('.album').animate({
+                                left:'-='+ count*$('.song').outerWidth(true)
+                            },400,function(){
+                                _this.isToStart = false; 
+                                _this.isAnimate = false;
+                                if((parseFloat($('.box').width())-parseFloat($('.album').css('left'))) >= $('.album').outerWidth(true)){
+                                    console.log('over');
+                                    _this.isAnimate = false;
+                                    _this.isToEnd = true;
+
+                                }
+                            })
+ 
+                        }
+                    }
                 })
 
                 $('.leftBtn').on('click',function(){
-                    console.log($('.album').css('left'));
-                    if(!_this.isToStart){
-                        $('.album').animate({
-                            left:'+='+ count*$('.song').outerWidth(true)
-                        },400,function(){
-                        _this.isToEnd = false;
-                        if(parseFloat($('.album').css('left')) >= 0){
-                            _this.isToStart = true;
+
+                    console.log(!_this.isAnimate);
+                    if(!_this.isAnimate){
+
+                        if(!_this.isToStart){
+                            _this.isAnimate = true;
+                            $('.album').animate({
+                                left:'+='+ count*$('.song').outerWidth(true)
+                            },400,function(){
+                                _this.isAnimate = false;
+                                _this.isToEnd = false;
+                                if(parseFloat($('.album').css('left')) >= 0){
+                                    _this.isAnimate = false;
+                                    _this.isToStart = true;
+
+                                }
+                            })
+
                         }
-                        })
                     }
                 })
+                console.log(_this.isAnimate);
+                console.log(_this.isToEnd);
 
         }).fail(function(){
             console.log('error')
